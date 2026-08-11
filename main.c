46d329496205b279d7b9a2e19fb006d55e17b88b
Levi <levivoth77@gmail.com>
2026-08-10 21:37:40 -0500

    add command line functionality

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "simple_inttypes.h"
 #include "file_phile.h"
@@ -8,26 +9,85 @@
 #include "random.h"
 #include "encrypt.h"
 
+typedef enum {
+    F,S
+} Mode;
+
 int main(int argc, char* argv[])
 {
+    int mode;
     int ret_val = 0;
+    u8* key = NULL;
 
-    if (argc != 3) {
-        printf("Usage: ./main <file path (data)> <file path (key)>\n");
+    if (argc != 2) {
+        printf("Usage: <file path (data)>\n");
         return 1;
     }
+
+    size_t buffer_size;
+    u8* buffer = load_file(argv[1], &buffer_size);
+    if (buffer == NULL) {
+        printf("Error: file %s not found\n", argv[1]);
+        return 1;
+    }
+
+    char key_input[128];
+    printf("Select input type [f]ile or [s]tring: ");
+    if (fgets(key_input, 128, stdin) == NULL) {
+        printf("Error reading argument\n");
+        ret_val = 1;
+        goto cleanup;
+    }
+
+    switch (key_input[0]) {
+        case 'f':
+            printf("Input file path: ");
+            mode = F;
+            break;
+        case 's':
+            printf("Input string: ");
+            mode = S;
+            break;
+        default:
+            printf("Invalid argument\n");
+            ret_val = 1;
+            goto cleanup;
+    }
+
+    if (fgets(key_input, 128, stdin) == NULL) {
+        printf("Error reading argument\n");
+        ret_val = 1;
+        goto cleanup;
+    }
+
+    size_t key_size;
+    if (mode == F) {
+        key = load_file(key_input, &key_size);
+    } else {
+        key = calloc(128, 1);
+    }
+
+    if (key == NULL) {
+        printf("Error: failed to allocate %s\n", key_input);
+        ret_val = 1;
+        goto cleanup;
+    }
+
+    if (mode == S) {
+        memcpy(key, key_input, 128);
+        for (key_size = 0; (key[key_size] != '\0') && key_size < 128; key_size++) {}
+        --key_size; // exclude \n character
+    }
+
+
+
+
 
     RandomContext context = {0};
 
-    size_t size;
-    u8* buffer = load_file(argv[2], &size);
-    if (buffer == NULL) {
-        printf("Error: file: %s not found\n", argv[2]);
-        return 1;
-    }
-
+    // get sha256 hash of keyfile
     u32 hash[8];
-    if (sha256(buffer, size, hash)) {
+    if (sha256(buffer, buffer_size, hash)) {
         printf("Error generating hash\n");
         ret_val = 1;
         goto cleanup;
@@ -39,20 +99,11 @@ int main(int argc, char* argv[])
         goto cleanup;
     }
 
-    if (random_get(&context, 0)) {
-        printf("Error\n");
-        ret_val = 1;
-        goto cleanup;
-    }
-
-    for (int i = 0; i < 8; i++) {
-        printf("%08x", context.data[i]);
-    }
-    printf("\n");
-
 
 cleanup:
     free(buffer); // unnesasary since the program is ending here anyways but just good practice
+    free(key);
     buffer = NULL;
+    key = NULL;
     return ret_val;
 }
--- random.c
+++ random.c
@@ -8,7 +8,7 @@
 #include "simple_inttypes.h"
 #include "sha256.h"
 
-static u64 x = 0;
+static u64 x = 0; // splitmix internal state
 
 status_t random_create(RandomContext* context, u32 new_key[8])
 {
@@ -25,7 +25,6 @@ status_t random_create(RandomContext* context, u32 new_key[8])
 status_t random_get(RandomContext* context, const u64 counter)
 {
     u8 hash_bytes[40];
-    u32 hash[8];
 
     if (context == NULL) {
         ERROR_REPORT(ERR_NULL);
@@ -35,12 +34,10 @@ status_t random_get(RandomContext* context, const u64 counter)
     memcpy(hash_bytes, context->key, 32);
     memcpy(hash_bytes + 32, &counter, 8);
 
-    if (sha256(hash_bytes, 40, hash)) {
+    if (sha256(hash_bytes, 40, context->data)) {
         ERROR_REPORT(ERR_SHA);
         return ERR_SHA;
     }
-
-    memcpy(context->data, hash, 32);
 
     return ERR_NONE;
 }
